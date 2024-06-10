49c0c82527536417a12a53916563818ef0fb4c5b
Vikalp Mishra <vikalpmishra783@gmail.com>
2024-06-10 10:46:30 +0530

    npm run build

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         "resources/css/app.css",
         "resources/js/app.js",
         "resources/js/admin.js",
+        "resources/css/admin.css",
       ],
       refresh: true,
     }),
